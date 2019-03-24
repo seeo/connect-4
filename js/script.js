ab21b341376cc37105f8ef1c70ea7c580b63e63d
seeo <aragonlotr5@gmail.com>
2019-03-24 17:40:58 +0800

    adding code to script out the horizontal lines based on size of the canvas

--- js/script.js
+++ js/script.js
@@ -22,9 +22,9 @@ window.addEventListener('mousemove',function(event){
 //orange color;  c.fillStyle = "#ffa500";
 // the rectangle ought to be taking directions from the width and height of the canvas: c.fillRect(87.5,50,525,450);
 // this is the x-cooridinate of the board position, beginning from the top left: console.log((700-(75*7))/2);
- // 87.5 = (canvas.width - (square * columns) / 2)
+ // 87.5 = ((canvas.width - (square * columns)) / 2)
 // this is the y-cooridinte of the board position, beginning from the top left: console.log((550-(75*6))/2);
-// 50 = (canvas.height - (square * rows) / 2)
+// 50 = ((canvas.height - (square * rows)) / 2)
 //525 = square * columns ; 450 = square * rows;
 
 //CREATING THE EMPTY STATE BOARD...
@@ -36,8 +36,21 @@ c.beginPath();
 //declare on the canvas where we want our path to start...
 //c.moveTo(x,y);
 c.strokeStyle = "#000000"; // black colored line;
-c.moveTo(87.5,125);
-c.lineTo(612.5,125);
+//c.moveTo(87.5,125);
+//first top line x-coordinate: (canvas.width - (square * columns) / 2)
+//first top line y-coordinate: (canvas.height - (square * rows) / 2) +square
+var x0y1_xCoordinate = ((canvas.width - (square * columns) )/ 2);
+var x0y1_yCoordinate = ((canvas.height - (square * rows)) / 2) + square;
+c.moveTo(x0y1_xCoordinate,x0y1_yCoordinate);
+//c.lineTo(612.5,125);
+// end of the x0 line, x7_xCoordinate: (x0_xCoordinate + (square * columns));
+//end of the line x0 line, x7_yCoordinate: (x0_yCoordinate);
+
+c.lineTo((x0y1_xCoordinate + (square * columns)),(x0y1_yCoordinate));
+//c.lineTo((((canvas.width - (square * columns))/2) + square*columns),(((canvas.height - (square * rows)) / 2) + square));
+
 c.stroke();
 //end the drawing of the first line, and begin on the next horizontal line;
 c.beginPath();
+//begin
+console.log("Javascript has ended!");
