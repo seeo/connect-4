29121a1f994b816148b1880e453cf19ad0926369
seeo <aragonlotr5@gmail.com>
2019-03-24 21:51:52 +0800

    changed the code so that squares and circles are created from the bottom left corner of the canvas.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+
+//orange color;  c.fillStyle = "#ffa500";
+// the rectangle ought to be taking directions from the width and height of the canvas: c.fillRect(87.5,50,525,450);
+// this is the x-cooridinate of the board position, beginning from the top left: console.log((700-(75*7))/2);
+ // 87.5 = ((canvas.width - (square * columns)) / 2)
+// this is the y-cooridinte of the board position, beginning from the top left: console.log((550-(75*6))/2);
+// 50 = ((canvas.height - (square * rows)) / 2)
+//525 = square * columns ; 450 = square * rows;
+//NOTE: We will deliberately start creating squares and circles from the bottom left corner, because the tokens will
+//drop to the bottom first due to 'gravity', therefore, indexing (aka counting) will begin from the bottom, and from the left.
+
+
 console.log("Javascript is linked!");
 // var c = document.getElementBy
 var canvas = document.querySelector("#myCanvas");
 var c = canvas.getContext('2d');
+//CREATING THE EMPTY STATE BOARD...
+//1. CREATE A BOARD WITH A BACKGROUND
 canvas.width = 700;
 canvas.height = 550;
 var square = 75; //length of one side of a square in pixels;
@@ -20,27 +34,17 @@ window.addEventListener('mousemove',function(event){
   console.log(mouse);
 })
 
-//orange color;  c.fillStyle = "#ffa500";
-// the rectangle ought to be taking directions from the width and height of the canvas: c.fillRect(87.5,50,525,450);
-// this is the x-cooridinate of the board position, beginning from the top left: console.log((700-(75*7))/2);
- // 87.5 = ((canvas.width - (square * columns)) / 2)
-// this is the y-cooridinte of the board position, beginning from the top left: console.log((550-(75*6))/2);
-// 50 = ((canvas.height - (square * rows)) / 2)
-//525 = square * columns ; 450 = square * rows;
-
-//CREATING THE EMPTY STATE BOARD...
-//1. CREATE A BOARD WITH A BACKGROUND
 
 //2.CREATE LINES FOR ROWS AND COLUMNS so you get illusion of seeing boxes
 //COORDINATES FOR SQUARES:
 var x0y0_xCoordinate = ((canvas.width - (square * columns) )/ 2);
-var x0y0_yCoordinate = ((canvas.height - (square * rows)) / 2);
+var x0y0_yCoordinate = (((canvas.height - (square * rows)) / 2)+(square *rows));
 console.log(x0y0_xCoordinate);
 console.log(x0y0_yCoordinate);
 
 //COORDINATES FOR CIRCLES:
 var x0y0_xCircle = x0y0_xCoordinate + (square/2);
-var x0y0_yCircle = x0y0_yCoordinate + (square/2);
+var x0y0_yCircle = x0y0_yCoordinate - (square/2);
 console.log(x0y0_xCoordinate);
 console.log(x0y0_yCoordinate);
 
@@ -56,11 +60,11 @@ function originBoard(){
 //c.moveTo(87.5,125);
 //first top line x-coordinate: (canvas.width - (square * columns) / 2)
 //first top line y-coordinate: (canvas.height - (square * rows) / 2) +square
-    c.moveTo(x0y0_xCoordinate,(x0y0_yCoordinate)+(square * i));
+    c.moveTo(x0y0_xCoordinate,(x0y0_yCoordinate)-(square * i));
 //c.lineTo(612.5,125);
 // end of the x0 line, x7_xCoordinate: (x0_xCoordinate + (square * columns));
 //end of the line x0 line, x7_yCoordinate: (x0_yCoordinate);
-    c.lineTo((x0y0_xCoordinate + (square * columns)),(x0y0_yCoordinate) + (square * i));
+    c.lineTo((x0y0_xCoordinate + (square * columns)),(x0y0_yCoordinate) - (square * i));
 //c.lineTo((((canvas.width - (square * columns))/2) + square*columns),(((canvas.height - (square * rows)) / 2) + square));
     console.log("Horizontal lines being drawn!");
     c.stroke();
@@ -71,14 +75,14 @@ function originBoard(){
     c.beginPath();
     c.strokeStyle = "#000000";
     c.moveTo((x0y0_xCoordinate)+(square * j),(x0y0_yCoordinate));
-    c.lineTo((x0y0_xCoordinate + (square * j)),(x0y0_yCoordinate) + (square * rows));
+    c.lineTo((x0y0_xCoordinate + (square * j)),(x0y0_yCoordinate) - (square * rows));
     c.stroke();
   }
-  //DRAW 'EMPTY' CIRCLES WITHIN THE SQUARES:
+//3. DRAW 'EMPTY' CIRCLES WITHIN THE SQUARES:
   //ADDING ROW and COLUMN OF CIRCLES:
   for (j = 0; j < rows; j++){
-    x0y0_yCircle = x0y0_yCoordinate + (square/2);
-    x0y0_yCircle += (square * j);
+    x0y0_yCircle = x0y0_yCoordinate - (square/2);
+    x0y0_yCircle -= (square * j);
     console.log(x0y0_yCircle);
     console.log("Rows of circles are being drawn");
   for (i= 0; i < columns; i++){
@@ -89,8 +93,10 @@ function originBoard(){
     }
   }
 }
-
+//4. CALL THE ORIGINAL BOARD FUNCTION HERE:
 originBoard();
+
+//Next we try to assign each circle an x- and y-coordinate identifier, using arrays.
 
 
 
