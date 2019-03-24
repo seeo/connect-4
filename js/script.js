56363dc93c43544fa74e7e41db8d3af98518cd24
seeo <aragonlotr5@gmail.com>
2019-03-24 12:02:00 +0800

    trying to draw grids on the canvas now. First horizontal upper most line drawn. Later to place circles

--- js/script.js
+++ js/script.js
@@ -8,25 +8,36 @@ var square = 75; //length of one side of a square in pixels;
 var columns = 7; //number of columns so we know how many squares we need horizontally;
 var rows = 6; // number of rows so that we know how many squares we need vertically;
 
-var mouse = { //created mouse object to check my x- and y-coordinates when I mouse over stuff;
+//CREATED A MOUSE OBJECT to check my x- and y-coordinates when I mouse over stuff;
+var mouse = {
   x: undefined,
   y: undefined
 }
-
-
-//creating the original game board;
- c.fillStyle = "#ffa500";
- c.fillRect(87.5,50,525,450); // the rectangle ought to be taking directions from the width and height of the canvas...
- console.log((700-(75*7))/2); // this is the x-cooridinate of the board position, beginning from the top left.
- // 87.5 = (canvas.width - (square * columns) / 2)
- console.log((550-(75*6))/2); // this is the y-cooridinte of the board position, beginning from the top left.
-// 50 = (canvas.height - (square * rows) / 2)
-//525 = square * columns ; 450 = square * rows;
-c.fillStyle = '#800080';
-c.fillRect((canvas.width - (square * columns))/ 2,(canvas.height - (square * rows)) / 2, square * columns, square * rows);
-
 window.addEventListener('mousemove',function(event){
   mouse.x = event.x;
   mouse.y = event.y;
   console.log(mouse);
 })
+
+//orange color;  c.fillStyle = "#ffa500";
+// the rectangle ought to be taking directions from the width and height of the canvas: c.fillRect(87.5,50,525,450);
+// this is the x-cooridinate of the board position, beginning from the top left: console.log((700-(75*7))/2);
+ // 87.5 = (canvas.width - (square * columns) / 2)
+// this is the y-cooridinte of the board position, beginning from the top left: console.log((550-(75*6))/2);
+// 50 = (canvas.height - (square * rows) / 2)
+//525 = square * columns ; 450 = square * rows;
+
+//CREATING THE EMPTY STATE BOARD...
+//1. CREATE A BOARD WITH A BACKGROUND
+c.fillStyle = '#fbca70'; //light orange color;
+c.fillRect((canvas.width - (square * columns))/ 2,(canvas.height - (square * rows)) / 2, square * columns, square * rows);
+//2.CREATE LINES FOR ROWS AND COLUMNS so you get illusion of seeing boxes
+c.beginPath();
+//declare on the canvas where we want our path to start...
+//c.moveTo(x,y);
+c.strokeStyle = "#000000"; // black colored line;
+c.moveTo(87.5,125);
+c.lineTo(612.5,125);
+c.stroke();
+//end the drawing of the first line, and begin on the next horizontal line;
+c.beginPath();
